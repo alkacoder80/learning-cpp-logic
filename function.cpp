c25dc0ae5f23446ee348439a9c5f4e300dc5b2a0
alkacoder80 <fenerabilli@gmail.com>
2026-02-02 22:06:55 +0800

    Update function.cpp

--- function.cpp
+++ function.cpp
@@ -1,4 +1,17 @@
-function tanpa return
-function dengan return
-parameter vs argument
-scope (local vs global)
+#include <iostream>
+using namespace std;
+
+int tambah(int a, int b) {
+    return a + b;
+}
+
+void cetakHasil(int hasil) {
+    cout << "Hasil: " << hasil << endl;
+}
+
+int main() {
+    int x = 5, y = 3;
+    int hasil = tambah(x, y);
+    cetakHasil(hasil);
+    return 0;
+}
